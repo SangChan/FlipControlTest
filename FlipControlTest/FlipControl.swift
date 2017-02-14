468f072a4956851e6800544cc73adb9d9dbeac69
Sangchan Lee <sangchan.lee@ef.com>
2017-02-14 18:04:45 +0800

    initial commit

---
+++ FlipControlTest/FlipControl.swift
@@ -0,0 +1,21 @@
+//
+//  FlipControl.swift
+//  FlipControlTest
+//
+//  Created by SangChan Lee on 2/13/17.
+//  Copyright © 2017 SangChan. All rights reserved.
+//
+
+import UIKit
+
+class FlipControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
